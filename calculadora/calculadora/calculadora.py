e66979607bfef4a3644ba84bb642a28bf2286c14
Deyved Antonio <deyved.antonio@gmail.com>
2023-09-14 13:52:23 -0300

    style: criar os widgets da tela

---
+++ calculadora/calculadora/calculadora.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+
+Window.size = 500, 400
+Window.clearcolor = (233/255, 255/255, 216/255, 1)
+
+
+class Home(BoxLayout):
+    pass
+
+
+class Calculadora(App):
+    def build(self):
+        return Home()
+
+
+if __name__ == '__main__':
+    Calculadora().run()
