444fded00f635447f37b79c521205feeae22d1a6
Deyved Antonio <deyved.antonio@gmail.com>
2023-09-26 14:32:49 -0300

    layout: mudanças na tela de login e tela home add.

--- agenda_clientes/agenda_clientes/main.py
+++ agenda_clientes/agenda_clientes/main.py
@@ -1,13 +1,13 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
 from sqlalchemy import (create_engine, MetaData, Column,
                         Table, Integer, String, DateTime)
 
 
-engine = create_engine('sqlite:///infra/agenda_clientes.db',
-                       echo=True)
+Window.size = 800, 500
+Window.clearcolor = (233/255, 233/255, 233/255, 1)
 
-metadata = MetaData(engine)
 
 class Main(App):
     def build(self):
@@ -19,6 +19,15 @@ class Gerenciador_telas(ScreenManager):
 
 
 class Login(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def verificar_status(self):
+        app = App.get_running_app()
+        app.root.current = 'tela_home'
+
+
+class Home(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
