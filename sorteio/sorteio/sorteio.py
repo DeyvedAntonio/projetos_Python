b69d2b296b27b7068ea3193a159e3a06b0fb23fb
Deyved Antonio <deyved.antonio@gmail.com>
2023-12-01 13:09:03 -0300

    refactor: organização do código.

--- sorteio/sorteio/sorteio.py
+++ sorteio/sorteio/sorteio.py
@@ -1,25 +1,5 @@
 import random
 
-lista_servidores = []
-restrições = {'segunda': [], 'terça': [], 'quarta': [], 'quinta': [], 'sexta': [], 'sabado': [], 'domingo': [], }
-fechamento = {'segunda': '', 'terça': '', 'quarta': '', 'quinta': '', 'sexta': '', 'sabado': '', 'domingo': '', }
-motorista = {'segunda': 'Thiago', 'terça': 'Thiago', 'quarta': 'Thiago', 'quinta': 'Thiago', 'sexta': 'Thiago', 'sabado': 'Thiago', 'domingo': 'Thiago', }
-carro_vagas = {'segunda': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
-               'terça': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
-               'quarta': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
-               'quinta': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
-               'sexta': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
-               'sabado': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
-               'domingo': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, }
-
-with open(r'sorteio\sorteio\arquivos\lista_servidores.csv', 'r') as file:
-    try:
-        lista_servidores = file.read().split(', ')
-    except FileNotFoundError:
-        print(f'Arquivo {file} não encontrado.')
-    else:
-        pass
-
 def dicionario_restrições() -> None:
     for chave in restrições.keys():
         restrições[chave] = input(f'Digite o nome do servidor que possui restrição na(o) {chave}: ').split(', ')
@@ -34,10 +14,30 @@ def modifica_motorista():
             motorista[dia] = 'Sergio'
 
 def sorteio():
-    for chave in carro_vagas.keys():
+    for chave, lista_vagas in carro_vagas.items():
         pass
 
 if __name__ == '__main__':
+    lista_servidores = []
+    restrições = {'segunda': [], 'terça': [], 'quarta': [], 'quinta': [], 'sexta': [], 'sabado': [], 'domingo': [], }
+    fechamento = {'segunda': '', 'terça': '', 'quarta': '', 'quinta': '', 'sexta': '', 'sabado': '', 'domingo': '', }
+    motorista = {'segunda': 'Thiago', 'terça': 'Thiago', 'quarta': 'Thiago', 'quinta': 'Thiago', 'sexta': 'Thiago', 'sabado': 'Thiago', 'domingo': 'Thiago', }
+    carro_vagas = {'segunda': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+                'terça': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+                'quarta': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+                'quinta': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+                'sexta': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+                'sabado': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+                'domingo': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, }
+
+    with open(r'sorteio\sorteio\arquivos\lista_servidores.csv', 'r') as file:
+        try:
+            lista_servidores = file.read().split(', ')
+        except FileNotFoundError:
+            print(f'Arquivo {file} não encontrado.')
+        else:
+            pass
+
     dicionario_fechamento()
     dicionario_restrições()
     modifica_motorista()
