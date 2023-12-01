c35c0603352dcfbe17c9e6956dc6dfaa9951c03a
Deyved Antonio <deyved.antonio@gmail.com>
2023-12-01 13:05:49 -0300

    feat: funções.

--- sorteio/sorteio/sorteio.py
+++ sorteio/sorteio/sorteio.py
@@ -2,9 +2,15 @@ import random
 
 lista_servidores = []
 restrições = {'segunda': [], 'terça': [], 'quarta': [], 'quinta': [], 'sexta': [], 'sabado': [], 'domingo': [], }
-fechamento = {'segunda': [], 'terça': [], 'quarta': [], 'quinta': [], 'sexta': [], 'sabado': [], 'domingo': [], }
-motorista = {'segunda': [], 'terça': [], 'quarta': [], 'quinta': [], 'sexta': [], 'sabado': [], 'domingo': [], }
-carro_vagas = {'segunda': [], 'terça': [], 'quarta': [], 'quinta': [], 'sexta': [], 'sabado': [], 'domingo': [], }
+fechamento = {'segunda': '', 'terça': '', 'quarta': '', 'quinta': '', 'sexta': '', 'sabado': '', 'domingo': '', }
+motorista = {'segunda': 'Thiago', 'terça': 'Thiago', 'quarta': 'Thiago', 'quinta': 'Thiago', 'sexta': 'Thiago', 'sabado': 'Thiago', 'domingo': 'Thiago', }
+carro_vagas = {'segunda': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+               'terça': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+               'quarta': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+               'quinta': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+               'sexta': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+               'sabado': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
+               'domingo': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, }
 
 with open(r'sorteio\sorteio\arquivos\lista_servidores.csv', 'r') as file:
     try:
@@ -14,17 +20,29 @@ with open(r'sorteio\sorteio\arquivos\lista_servidores.csv', 'r') as file:
     else:
         pass
 
-def dicionario_restrições():
+def dicionario_restrições() -> None:
     for chave in restrições.keys():
-        restrições[chave] = input('Digite o nome do servidor: ').split(', ')
+        restrições[chave] = input(f'Digite o nome do servidor que possui restrição na(o) {chave}: ').split(', ')
 
-def dicionario_fechamento():
+def dicionario_fechamento() -> None:
     for chave in fechamento.keys():
-        fechamento[chave] = input('Digite o nome do sevidor: ')
+        fechamento[chave] = input(f'Digite o nome do sevidor que vai fechar na(o) {chave}: ')
+
+def modifica_motorista():
+    for dia, servidor in fechamento.items():
+        if servidor == motorista[dia]:
+            motorista[dia] = 'Sergio'
 
 def sorteio():
     for chave in carro_vagas.keys():
         pass
 
-print(lista_servidores)
-print(restrições)
+if __name__ == '__main__':
+    dicionario_fechamento()
+    dicionario_restrições()
+    modifica_motorista()
+
+    print(f'restrições: {restrições}\n')
+    print(f'fechamento: {fechamento}\n')
+    print(f'motorista: {motorista}\n')
+    print(f'Vagas: {carro_vagas}')
