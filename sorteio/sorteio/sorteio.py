ca8166f35c88e0481e17dcf5946ad3062e6a7692
Deyved Antonio <deyved.antonio@gmail.com>
2023-12-01 13:15:39 -0300

    refactor: ajustes nas assinaturas das funções.

--- sorteio/sorteio/sorteio.py
+++ sorteio/sorteio/sorteio.py
@@ -1,17 +1,17 @@
 import random
 
-def dicionario_restrições() -> None:
-    for chave in restrições.keys():
-        restrições[chave] = input(f'Digite o nome do servidor que possui restrição na(o) {chave}: ').split(', ')
+def dicionario_restrições(listagem) -> None:
+    for chave in listagem.keys():
+        listagem[chave] = input(f'Digite o nome do servidor que possui restrição na(o) {chave}: ').split(', ')
 
-def dicionario_fechamento() -> None:
-    for chave in fechamento.keys():
-        fechamento[chave] = input(f'Digite o nome do sevidor que vai fechar na(o) {chave}: ')
+def dicionario_fechamento(listagem) -> None:
+    for chave in listagem.keys():
+        listagem[chave] = input(f'Digite o nome do sevidor que vai fechar na(o) {chave}: ')
 
-def modifica_motorista():
-    for dia, servidor in fechamento.items():
-        if servidor == motorista[dia]:
-            motorista[dia] = 'Sergio'
+def modifica_motorista(listagem_fechamento, motoristas):
+    for dia, servidor in listagem_fechamento.items():
+        if servidor == motoristas[dia]:
+            motoristas[dia] = 'Sergio'
 
 def sorteio():
     for chave, lista_vagas in carro_vagas.items():
@@ -20,8 +20,8 @@ def sorteio():
 if __name__ == '__main__':
     lista_servidores = []
     restrições = {'segunda': [], 'terça': [], 'quarta': [], 'quinta': [], 'sexta': [], 'sabado': [], 'domingo': [], }
-    fechamento = {'segunda': '', 'terça': '', 'quarta': '', 'quinta': '', 'sexta': '', 'sabado': '', 'domingo': '', }
-    motorista = {'segunda': 'Thiago', 'terça': 'Thiago', 'quarta': 'Thiago', 'quinta': 'Thiago', 'sexta': 'Thiago', 'sabado': 'Thiago', 'domingo': 'Thiago', }
+    fechamentos = {'segunda': '', 'terça': '', 'quarta': '', 'quinta': '', 'sexta': '', 'sabado': '', 'domingo': '', }
+    motoristas = {'segunda': 'Thiago', 'terça': 'Thiago', 'quarta': 'Thiago', 'quinta': 'Thiago', 'sexta': 'Thiago', 'sabado': 'Thiago', 'domingo': 'Thiago', }
     carro_vagas = {'segunda': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
                 'terça': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
                 'quarta': {'Vaga1': '', 'Vaga2': '', 'Vaga3': '', 'Vaga4': ''}, 
@@ -38,11 +38,11 @@ if __name__ == '__main__':
         else:
             pass
 
-    dicionario_fechamento()
-    dicionario_restrições()
-    modifica_motorista()
+    dicionario_fechamento(fechamentos)
+    dicionario_restrições(restrições)
+    modifica_motorista(fechamentos, motoristas)
 
     print(f'restrições: {restrições}\n')
-    print(f'fechamento: {fechamento}\n')
-    print(f'motorista: {motorista}\n')
+    print(f'fechamento: {fechamentos}\n')
+    print(f'motorista: {motoristas}\n')
     print(f'Vagas: {carro_vagas}')
