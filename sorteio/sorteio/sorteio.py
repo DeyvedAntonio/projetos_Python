0ba6621684df5f44e978cdd2e24ffce9648d8166
Deyved Antonio <deyved.antonio@gmail.com>
2023-12-27 17:13:18 -0300

    feat: função definir motorista.

--- sorteio/sorteio/sorteio.py
+++ sorteio/sorteio/sorteio.py
@@ -4,7 +4,10 @@ import random
 vagas_carro = 4
 caminho = os.getcwd()
 lista_fechamento = {'segunda': '', 'terca': '', 'quarta': '', 'quinta': '', 'sexta': '', 'sabado': '', 'domingo': '',}
+lista_vagas = {'segunda': [], 'terca': [], 'quarta': [], 'quinta': [], 'sexta': [], 'sabado': [], 'domingo': [],}
+restricoes = {'segunda': [], 'terca': [], 'quarta': [], 'quinta': [], 'sexta': [], 'sabado': [], 'domingo': [],}
 lista_servidores = []
+motoristas = {'segunda': '', 'terca': '', 'quarta': '', 'quinta': '', 'sexta': '', 'sabado': '', 'domingo': '',}
 
 try:
     with open(f'{caminho}\\sorteio\\arquivos\\lista_servidores.csv', 'r') as file:
@@ -16,9 +19,16 @@ def definir_fechamento():
     for chave in lista_fechamento.keys():
         lista_fechamento[chave] = input(f'Informe o nome do servidor responsável pelo fechamento no(a) {chave} ').strip().title()
 
+def definir_motorista():
+    for chave in motoristas.keys():
+        motoristas[chave] = input(f'Infome o motorista no(a) {chave} ').strip().title()
+
 def sorteio_nomes(quantidade=vagas_carro):
     sorteados = random.choices(lista_servidores, k=quantidade)
 
+def verificar_fechamento(nomes):
+    pass
 
 definir_fechamento()
+
 sorteio_nomes()
